cbaa5fd4c053b88d7fa3b9b29d56e6c2c89a8d80
bvictorb <victory-boucher@hotmail.com>
2020-12-04 12:41:33 +0100

    fixed linkks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,20 +14,20 @@ const Footer = () => {
               <img className="m-footer__image" src={sam} alt=""/>
               <h3>Sam Lansdaal</h3>
               <h4>Visual & animation artist</h4>
-              <a target="_blank" href="https://samlansdaal.myportfolio.com/"><img className="m-footer__icon" src={globe} alt=""/></a>
+              <a rel="noreferrer" target="_blank" href="https://samlansdaal.myportfolio.com/"><img className="m-footer__icon" src={globe} alt=""/></a>
             </div>
             <div>
               <img className="m-footer__image" src={victor} alt=""/>
               <h3>Victor Boucher</h3>
               <h4>Frontend developer & project management</h4>
-              <a target="_blank" href="https://www.linkedin.com/in/victor-boucher-18267813b/"><img className="m-footer__icon" src={linkedin} alt=""/></a>
-              <a target="_blank" href="https://github.com/BVictorB"><img className="m-footer__icon" src={github} alt=""/></a>
+              <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/victor-boucher-18267813b/"><img className="m-footer__icon" src={linkedin} alt=""/></a>
+              <a rel="noreferrer" target="_blank" href="https://github.com/BVictorB"><img className="m-footer__icon" src={github} alt=""/></a>
             </div>
             <div>
               <img className="m-footer__image" src={gosro} alt=""/>
               <h3>Gosro Poorheydar</h3>
               <h4>Visual artist & storyteller</h4>
-              <a target="_blank" href="https://gosro-poorheydar.webnode.nl/"><img className="m-footer__icon" src={globe} alt=""/></a>
+              <a rel="noreferrer" target="_blank" href="https://gosro-poorheydar.webnode.nl/"><img className="m-footer__icon" src={globe} alt=""/></a>
             </div>
           </div>
         </div>
